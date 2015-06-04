48506332ba5e9e3efa09d9585e1f9d43e0fe6869
daniel <abrahamdaniel92@gmail.com>
2015-06-04 19:05:13 +0530

    Added spec to check instance creation

--- src/main/java/com/biblioteca/dao/BookDAO.java
+++ src/main/java/com/biblioteca/dao/BookDAO.java
@@ -1,4 +1,7 @@
 package com.biblioteca.dao;
 
 public class BookDAO {
+    public static BookDAO getInstance() {
+        return new BookDAO();
+    }
 }
--- src/test/java/com/biblioteca/dao/BookDAOTest.java
+++ src/test/java/com/biblioteca/dao/BookDAOTest.java
@@ -2,12 +2,16 @@ package com.biblioteca.dao;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class BookDAOTest {
 
     @Test
-    public void testGetInstance() throws Exception {
+    public void shouldGetInstanceOfBookDAO() throws Exception {
+        BookDAO bookDAO = BookDAO.getInstance();
 
+        assertThat(bookDAO, instanceOf(BookDAO.class));
     }
 }
