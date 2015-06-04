9167a2dd0bdfe2559896f8b91c74c5839aa79f4c
daniel <abrahamdaniel92@gmail.com>
2015-06-04 21:31:24 +0530

    Refactored BookDAOTest to inculde a setup method

--- src/test/java/com/biblioteca/dao/BookDAOTest.java
+++ src/test/java/com/biblioteca/dao/BookDAOTest.java
@@ -1,6 +1,10 @@
 package com.biblioteca.dao;
 
+import com.biblioteca.model.Book;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.core.Is.is;
@@ -8,18 +12,26 @@ import static org.junit.Assert.*;
 
 public class BookDAOTest {
 
+    BookDAO bookDAO;
+
+    @Before
+    public void setUp() {
+        bookDAO = BookDAO.getInstance();
+    }
+
     @Test
     public void shouldGetInstanceOfBookDAO() {
-        BookDAO bookDAO = BookDAO.getInstance();
+        bookDAO = BookDAO.getInstance();
 
         assertThat(bookDAO, instanceOf(BookDAO.class));
     }
 
     @Test
     public void shouldGetSingletonInstanceOfBookDAO() {
-        BookDAO firstBookDAO = BookDAO.getInstance();
+        bookDAO = BookDAO.getInstance();
         BookDAO secondBookDAO = BookDAO.getInstance();
 
-        assertThat(firstBookDAO, is(secondBookDAO));
+        assertThat(bookDAO, is(secondBookDAO));
     }
+
 }
