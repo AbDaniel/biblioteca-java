64e7cdd783fb20c697c82597e9bcda19dfc02145
daniel <abrahamdaniel92@gmail.com>
2015-06-04 19:13:39 +0530

    Refactored Main to accomadate change in Biblioteca app

--- src/main/java/com/biblioteca/app/Main.java
+++ src/main/java/com/biblioteca/app/Main.java
@@ -1,10 +1,11 @@
 package com.biblioteca.app;
 
+import com.biblioteca.dao.BookDAO;
 
 public class Main {
 
     public static void main( String[] args) {
-        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        BibliotecaApp bibliotecaApp = new BibliotecaApp(BibliotecaApp.WELCOME_TEXT, BookDAO.getInstance());
     }
 
 }
