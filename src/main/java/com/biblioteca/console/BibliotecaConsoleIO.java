d8e947d6fc8c1facaaa46819349527b807e80644
daniel <abrahamdaniel92@gmail.com>
2015-06-04 20:30:40 +0530

    Added BibliotecaConsoleIO class for I/O operations

---
+++ src/main/java/com/biblioteca/console/BibliotecaConsoleIO.java
@@ -0,0 +1,9 @@
+package com.biblioteca.console;
+
+public class BibliotecaConsoleIO {
+
+    public void displayWelcomeMessage(String welcomeMessage) {
+        System.out.println(welcomeMessage);
+    }
+
+}
