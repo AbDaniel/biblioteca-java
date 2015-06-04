f7c53163de1fd12da5d7336b8b57875d3f14be22
daniel <abrahamdaniel92@gmail.com>
2015-06-04 21:38:23 +0530

    Added method to display list of books in BibliotecaConsoleIO class

--- src/main/java/com/biblioteca/console/BibliotecaConsoleIO.java
+++ src/main/java/com/biblioteca/console/BibliotecaConsoleIO.java
@@ -1,9 +1,19 @@
 package com.biblioteca.console;
 
+import com.biblioteca.model.Book;
+
+import java.util.List;
+
 public class BibliotecaConsoleIO {
 
     public void displayWelcomeMessage(String welcomeMessage) {
         System.out.println(welcomeMessage);
+    }
+
+    public void displayListOfBooks(List<Book> books) {
+        for (Book book : books) {
+            System.out.println(book);
+        }
     }
 
 }
