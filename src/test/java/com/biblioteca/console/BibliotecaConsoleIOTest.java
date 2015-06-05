c9ba4635a5d3d3f09c646182e5a620157bfc04b4
daniel <abrahamdaniel92@gmail.com>
2015-06-06 00:05:45 +0530

    Added Spec to test display welcome message in BiblioTecaConsoleIo

--- src/test/java/com/biblioteca/console/BibliotecaConsoleIOTest.java
+++ src/test/java/com/biblioteca/console/BibliotecaConsoleIOTest.java
@@ -1,7 +1,35 @@
 package com.biblioteca.console;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+
+import static com.biblioteca.app.BibliotecaApp.*;
+import static org.junit.Assert.assertEquals;
 
 public class BibliotecaConsoleIOTest {
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
+    BibliotecaConsoleIO bibliotecaConsoleIO;
+
+    @Before
+    public void setUp() {
+        bibliotecaConsoleIO = new BibliotecaConsoleIO();
+    }
+
+    @Test
+    public void shouldDisplayWelcomeMessage() {
+        bibliotecaConsoleIO.displayWelcomeMessage(WELCOME_TEXT);
+
+        assertEquals(WELCOME_TEXT + "\n", systemOutRule.getLog());
+    }
+
+    @Test
+    public void shouldDisplayListOfBooks() {
+
+    }
 
 }
