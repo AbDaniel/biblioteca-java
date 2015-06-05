c4ffe73d71e8a7c7988e22f5ee51e15c6eb8dcba
daniel <abrahamdaniel92@gmail.com>
2015-06-06 00:48:37 +0530

    Added MenuEnum

---
+++ src/main/java/com/biblioteca/enums/MenuEnum.java
@@ -0,0 +1,24 @@
+package com.biblioteca.enums;
+
+public enum MenuEnum {
+
+    LIST_BOOKS(1, "List all books"),
+    QUIT(2, "Quit Biblioteca!! (Highly Recommended!!)");
+
+    private int code;
+    private String text;
+
+    MenuEnum(int code, String text) {
+        this.code = code;
+        this.text = text;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+}
