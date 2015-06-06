5731ae2532784bc4d96fd6aa3b79f13d963a3e18
daniel <abrahamdaniel92@gmail.com>
2015-06-06 10:21:40 +0530

    Added spec to test toString of Book

--- src/test/java/com/biblioteca/model/BookTest.java
+++ src/test/java/com/biblioteca/model/BookTest.java
@@ -1,13 +1,26 @@
 package com.biblioteca.model;
 
+import com.biblioteca.enums.MenuItem;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BookTest {
 
     @Test
     public void equalsContract() {
         EqualsVerifier.forClass(Book.class).usingGetClass().verify();
+    }
+
+    @Test
+    public void shouldReturnFormatedStringToRespresentBookState() {
+        Book book = new Book("Lord of the Rings", "JR Toliken", 1930);
+
+        String actualString = book.toString();
+        String expectedString = "name='Lord of the Rings', author='JR Toliken', year=1930";
+
+        assertEquals(expectedString, actualString);
     }
 
 }
