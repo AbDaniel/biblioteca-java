0710f6612b295b2aeb90b4325e43f5a555313c37
daniel <abrahamdaniel92@gmail.com>
2015-06-07 18:46:43 +0530

    1. Added ReturnCommand and ReturnCommandTest 2. Added spec to test is message is displayed asking user for book name in ReturnCommand

---
+++ src/main/java/com/biblioteca/command/ReturnCommand.java
@@ -0,0 +1,24 @@
+package com.biblioteca.command;
+
+import com.biblioteca.console.BibliotecaConsoleIO;
+import com.biblioteca.repository.Books;
+
+import static com.biblioteca.console.BibliotecaConsoleIO.CHECKOUT_PROMPT_TEXT;
+
+public class ReturnCommand implements Command {
+
+    private Books books;
+    private BibliotecaConsoleIO bibliotecaConsoleIO;
+
+    public ReturnCommand(Books books, BibliotecaConsoleIO bibliotecaConsoleIO) {
+        this.books = books;
+        this.bibliotecaConsoleIO = bibliotecaConsoleIO;
+    }
+
+    @Override
+    public void execute() {
+        bibliotecaConsoleIO.displayMessage(CHECKOUT_PROMPT_TEXT);
+        String bookName = bibliotecaConsoleIO.getBookNameFromUser();
+    }
+
+}
---
+++ src/test/java/com/biblioteca/command/ReturnCommandTest.java
@@ -0,0 +1,37 @@
+package com.biblioteca.command;
+
+import com.biblioteca.console.BibliotecaConsoleIO;
+import com.biblioteca.repository.Books;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static com.biblioteca.console.BibliotecaConsoleIO.CHECKOUT_PROMPT_TEXT;
+import static org.mockito.Mockito.when;
+
+public class ReturnCommandTest {
+
+    @Mock
+    BibliotecaConsoleIO bibliotecaConsoleIO;
+
+    @Mock
+    private Books books;
+
+    private ReturnCommand command;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        command = new ReturnCommand(books, bibliotecaConsoleIO);
+    }
+
+    @Test
+    public void shouldDisplayMessageAskingUserToInputBookName() {
+        command.execute();
+
+        Mockito.verify(bibliotecaConsoleIO).displayMessage(CHECKOUT_PROMPT_TEXT);
+    }
+
+}
