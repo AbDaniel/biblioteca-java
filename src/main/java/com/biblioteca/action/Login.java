7f98c274517633eb3248e19e99dcacdab6c41ca0
daniel <abrahamdaniel92@gmail.com>
2015-06-11 17:59:28 +0530

    Added Login class and its spec

---
+++ src/main/java/com/biblioteca/action/Login.java
@@ -0,0 +1,18 @@
+package com.biblioteca.action;
+
+import com.biblioteca.model.User;
+
+import java.util.Map;
+
+public class Login {
+
+    private Map<String, String> users;
+
+    public Login(Map<String, String> users) {
+        this.users = users;
+    }
+
+    public User login() {
+        return null;
+    }
+}
---
+++ src/test/java/com/biblioteca/action/LoginTest.java
@@ -0,0 +1,30 @@
+package com.biblioteca.action;
+
+import com.biblioteca.model.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class LoginTest {
+
+    Login login;
+
+    @Before
+    public void setUp() throws Exception {
+        Map<String, String> users = new HashMap<>();
+        users.put("111-1111", "Sauron");
+        login = new Login(users);
+    }
+
+    @Test
+    public void shouldReturnNullOnInvalidLogin() {
+        User user = login.login();
+
+        assertNull(user);
+    }
+
+}
