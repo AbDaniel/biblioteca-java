b0e136fe2b90758e76d1e63384e0c80bebdd1c3b
daniel <abrahamdaniel92@gmail.com>
2015-06-12 01:08:29 +0530

    Added BorrowablesListView and its spec

---
+++ src/main/java/com/biblioteca/console/BorrowablesListView.java
@@ -0,0 +1,20 @@
+package com.biblioteca.console;
+
+import com.biblioteca.model.Borrowable;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class BorrowablesListView extends View {
+
+    public BorrowablesListView(Scanner scanner) {
+        super(scanner);
+    }
+
+    public void displayListOfBorrowables(List<? extends Borrowable> borrowables) {
+        for (Borrowable borrowable : borrowables) {
+            System.out.println(borrowable);
+        }
+    }
+
+}
---
+++ src/test/java/com/biblioteca/console/BorrowablesListViewTest.java
@@ -0,0 +1,50 @@
+package com.biblioteca.console;
+
+import com.biblioteca.model.Book;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+
+public class BorrowablesListViewTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Rule
+    public final TextFromStandardInputStream systemInMock
+            = TextFromStandardInputStream.emptyStandardInputStream();
+
+    BorrowablesListView borrowablesListView;
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+        borrowablesListView = new BorrowablesListView(new Scanner(System.in));
+    }
+
+    @Test
+    public void shouldDisplayList() {
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("Lord of the Rings", "JR Toliken", 1930));
+        books.add(new Book("Harry Potter", "JK Rowling", 1992));
+        books.add(new Book("Catch-22", "Joesph Heller", 1950));
+        books.add(new Book("Winds of Winter", "George RR Martin", 2017));
+
+        borrowablesListView.displayListOfBorrowables(books);
+        String expected = "name='Lord of the Rings', author='JR Toliken', year=1930\n" +
+                "name='Harry Potter', author='JK Rowling', year=1992\n" +
+                "name='Catch-22', author='Joesph Heller', year=1950\n" +
+                "name='Winds of Winter', author='George RR Martin', year=2017\n";
+
+        assertEquals(expected, outContent.toString());
+    }
+
+}
