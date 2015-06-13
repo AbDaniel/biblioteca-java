80b2e854aac06a51ad2256c6f51ce4c02f26a06d
daniel <abrahamdaniel92@gmail.com>
2015-06-14 00:07:38 +0530

    Added MovieVisitor class and its specs

--- src/main/java/com/biblioteca/visitor/MovieVisitor.java
+++ src/main/java/com/biblioteca/visitor/MovieVisitor.java
@@ -1,4 +1,33 @@
 package com.biblioteca.visitor;
 
-public class MovieVisitor {
+import com.biblioteca.model.Movie;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class MovieVisitor implements Visitor {
+
+    private List<Movie> movies;
+    private Function<Movie, String> format;
+
+    public MovieVisitor(List<Movie> movies, Function<Movie, String> format) {
+        this.format = format;
+        this.movies = movies;
+    }
+
+    @Override
+    public void visit(Movie movie) {
+        movies.add(movie);
+    }
+
+    @Override
+    public String visitables() {
+        StringBuilder builder = new StringBuilder();
+        movies.forEach(movie -> builder.append(movie.toString(format)).append("\n"));
+        return builder.toString();
+    }
+
+    public int size() {
+        return movies.size();
+    }
 }
--- src/test/java/com/biblioteca/visitor/MovieVisitorTest.java
+++ src/test/java/com/biblioteca/visitor/MovieVisitorTest.java
@@ -1,9 +1,46 @@
 package com.biblioteca.visitor;
 
+import com.biblioteca.model.Book;
+import com.biblioteca.model.Movie;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.biblioteca.model.Movie.REGULAR_MOVIE_FORMAT;
+import static junit.framework.TestCase.assertEquals;
+
 
 public class MovieVisitorTest {
 
+    MovieVisitor visitor;
+
+    @Before
+    public void setUp() throws Exception {
+        List<Visitable> visitables = new ArrayList<>();
+        visitables.add(new Book("Lord of the Rings", "JR Toliken", 1930));
+        visitables.add(new Book("Harry Potter", "JK Rowling", 1992));
+        visitables.add(new Book("Catch-22", "Joesph Heller", 1950));
+        visitables.add(new Book("Winds of Winter", "George RR Martin", 2017));
+        visitables.add(new Movie("The Matrix", "The Wachowskis", 1999, 10));
+        visitor = new MovieVisitor(new ArrayList<>(), REGULAR_MOVIE_FORMAT);
+        visitables.forEach(visitable -> visitable.accept(visitor));
+    }
+
+    @Test
+    public void shouldReturnMovieAsString() {
+        String actualString = visitor.visitables();
+
+        String expectedString = "name='The Matrix', director='The Wachowskis', year=1999, rating=10\n";
+
+        assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    public void shouldReturnSizeOfVisitablesVisited() {
+        int actualSize = visitor.size();
+
+        assertEquals(1, actualSize);
+    }
 }
